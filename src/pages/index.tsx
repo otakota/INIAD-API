2de8faf7cd753a4bd42a17d638733e84830de216
otakota <s1f102302685@iniad.org>
2024-05-21 20:40:43 +0900

    ugoita!

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,42 @@
+import { useState } from 'react';
 import styles from './index.module.css';
+import { EduIotApiClient } from 'iniad-api-client';
 
 const Home = () => {
-  return <div className={styles.container} />;
+  const [userName, setUserName] = useState('');
+  const [passWord, setPassWord] = useState('');
+
+  const lockerOpen = async () => {
+    const iotClient = new EduIotApiClient(userName, passWord, 'https://edu-iot.iniad.org');
+    try {
+      const res = await iotClient.openLocker();
+      console.log(res);
+    } catch {
+      console.error('error');
+    }
+  };
+
+  return (
+    <div className={styles.container}>
+      <input
+        type="text"
+        placeholder="otako"
+        onChange={(event) => {
+          setUserName(event.currentTarget.value);
+        }}
+        value={userName}
+      />
+      <input
+        type="password"
+        placeholder="ta"
+        onChange={(event) => {
+          setPassWord(event.currentTarget.value);
+        }}
+        value={passWord}
+      />
+      <button onClick={lockerOpen}>open</button>
+    </div>
+  );
 };
 
 export default Home;
