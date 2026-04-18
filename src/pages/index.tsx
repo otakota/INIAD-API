91ae55ae6c064c42c9113da63a948639de9dee8c
otakota <otakota268@gmail.com>
2026-04-19 00:54:09 +0900

    add register icCard

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,12 @@ import { useState } from 'react';
 import styles from './index.module.css';
 import { EduIotApiClient } from 'iniad-api-client';
 
+
 const Home = () => {
   const [userName, setUserName] = useState('');
   const [passWord, setPassWord] = useState('');
+  const [icCardId, setIcCardId] = useState('');
+  const [comment, setComment] = useState('');
 
   const lockerOpen = async () => {
     const iotClient = new EduIotApiClient(userName, passWord, 'https://edu-iot.iniad.org');
@@ -16,8 +19,19 @@ const Home = () => {
     }
   };
 
+  const registerIcCard = async () =>{
+    const iotClient = new EduIotApiClient(userName, passWord, 'https://edu-iot.iniad.org');
+    try{
+      const res = await iotClient.registerICCard(icCardId, comment);
+      console.log(res);
+    } catch{
+      console.error('error');
+    }
+  }
+
   return (
     <div className={styles.container}>
+      <section>
       <input
         type="text"
         placeholder="username"
@@ -34,8 +48,30 @@ const Home = () => {
         }}
         value={passWord}
       />
-      <button onClick={lockerOpen}>open</button>
+      <button onClick={lockerOpen}>ロッカーを開ける</button>
+      </section>
+
+      <section>
+        <input
+        type="text"
+        placeholder='icCardId'
+        onChange={(event)=>{
+          setIcCardId(event.currentTarget.value);
+        }}
+        value={icCardId}
+        />
+        <input
+        type="text"
+        placeholder='comment'
+        onChange={(event)=>{
+          setComment(event.currentTarget.value);
+        }}
+        value={comment}
+        />
+        <button onClick={registerIcCard}>ICカードを登録</button>
+      </section>
     </div>
+
   );
 };
 
