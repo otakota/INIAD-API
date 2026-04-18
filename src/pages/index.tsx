5e689c476bf022133394c85ea77a34ba25c30d81
otakota <otakota268@gmail.com>
2026-04-19 01:03:59 +0900

    lint files

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,6 @@ import { useState } from 'react';
 import styles from './index.module.css';
 import { EduIotApiClient } from 'iniad-api-client';
 
-
 const Home = () => {
   const [userName, setUserName] = useState('');
   const [passWord, setPassWord] = useState('');
@@ -11,6 +10,7 @@ const Home = () => {
 
   const lockerOpen = async () => {
     const iotClient = new EduIotApiClient(userName, passWord, 'https://edu-iot.iniad.org');
+
     try {
       const res = await iotClient.openLocker();
       console.log(res);
@@ -19,59 +19,59 @@ const Home = () => {
     }
   };
 
-  const registerIcCard = async () =>{
+  const registerIcCard = async () => {
     const iotClient = new EduIotApiClient(userName, passWord, 'https://edu-iot.iniad.org');
-    try{
+
+    try {
       const res = await iotClient.registerICCard(icCardId, comment);
       console.log(res);
-    } catch{
+    } catch {
       console.error('error');
     }
-  }
+  };
 
   return (
     <div className={styles.container}>
       <section>
-      <input
-        type="text"
-        placeholder="username"
-        onChange={(event) => {
-          setUserName(event.currentTarget.value);
-        }}
-        value={userName}
-      />
-      <input
-        type="password"
-        placeholder="password"
-        onChange={(event) => {
-          setPassWord(event.currentTarget.value);
-        }}
-        value={passWord}
-      />
-      <button onClick={lockerOpen}>ロッカーを開ける</button>
+        <input
+          type="text"
+          placeholder="username"
+          onChange={(event) => {
+            setUserName(event.currentTarget.value);
+          }}
+          value={userName}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          onChange={(event) => {
+            setPassWord(event.currentTarget.value);
+          }}
+          value={passWord}
+        />
+        <button onClick={lockerOpen}>ロッカーを開ける</button>
       </section>
 
       <section>
         <input
-        type="text"
-        placeholder='icCardId'
-        onChange={(event)=>{
-          setIcCardId(event.currentTarget.value);
-        }}
-        value={icCardId}
+          type="text"
+          placeholder="icCardId"
+          onChange={(event) => {
+            setIcCardId(event.currentTarget.value);
+          }}
+          value={icCardId}
         />
         <input
-        type="text"
-        placeholder='comment'
-        onChange={(event)=>{
-          setComment(event.currentTarget.value);
-        }}
-        value={comment}
+          type="text"
+          placeholder="comment"
+          onChange={(event) => {
+            setComment(event.currentTarget.value);
+          }}
+          value={comment}
         />
         <button onClick={registerIcCard}>ICカードを登録</button>
       </section>
     </div>
-
   );
 };
 
